b7880391be708598f2fe1b6f05274dd8d46e7548
Ivan Šarić <ivan@path-variable.com>
2022-09-09 15:39:56 +0200

    refactors code and tests, improves docs (#2)

--- pkg/p_time/time_test.go
+++ pkg/p_time/time_test.go
@@ -9,39 +9,29 @@ const expectedCET = "CET-1CEST,M3.5.0/2,M10.5.0/3"
 const expectedEST = "EST5EDT,M3.2.0/2,M11.1.0"
 const zagrebZone = "Europe/Zagreb"
 const newyorkZone = "America/New_York"
+const errorTemplate = "Time zone string for test time %s is not correct. Expected %s but got %s"
 
 func TestFormatTimeZoneCET(t *testing.T) {
-	loc, _ := time.LoadLocation(zagrebZone)
-	testTime := time.Date(2022, 4, 12, 0, 0, 0, 0, loc)
-	formatted := FormatTimeZone(testTime)
-	if formatted != expectedCET {
-		t.Fatalf("Time zone string for test time %s is not correct. Expected %s but got %s", testTime, expectedCET, formatted)
-	}
+	testFormatTimeZone(t, zagrebZone, expectedCET, 2022, 4, 12)
 }
 
 func TestFormatTimeZoneCETDSTFirst(t *testing.T) {
-	loc, _ := time.LoadLocation(zagrebZone)
-	testTime := time.Date(2022, 7, 12, 0, 0, 0, 0, loc)
-	formatted := FormatTimeZone(testTime)
-	if formatted != expectedCET {
-		t.Fatalf("Time zone string for test time %s is not correct. Expected %s but got %s", testTime, expectedCET, formatted)
-	}
+	testFormatTimeZone(t, zagrebZone, expectedCET, 2022, 7, 12)
 }
 
 func TestFormatTimeZoneEST(t *testing.T) {
-	loc, _ := time.LoadLocation(newyorkZone)
-	testTime := time.Date(2022, 4, 12, 0, 0, 0, 0, loc)
-	formatted := FormatTimeZone(testTime)
-	if formatted != expectedEST {
-		t.Fatalf("Time zone string for test time %s is not correct. Expected %s but got %s", testTime, expectedEST, formatted)
-	}
+	testFormatTimeZone(t, newyorkZone, expectedEST, 2022, 4, 12)
 }
 
 func TestFormatTimeZoneESTDSTFirst(t *testing.T) {
-	loc, _ := time.LoadLocation(newyorkZone)
-	testTime := time.Date(2022, 7, 12, 0, 0, 0, 0, loc)
+	testFormatTimeZone(t, newyorkZone, expectedEST, 2022, 7, 12)
+}
+
+func testFormatTimeZone(t *testing.T, location, expectedFormat string, year, month, day int) {
+	loc, _ := time.LoadLocation(location)
+	testTime := time.Date(year, time.Month(month), day, 0, 0, 0, 0, loc)
 	formatted := FormatTimeZone(testTime)
-	if formatted != expectedEST {
-		t.Fatalf("Time zone string for test time %s is not correct. Expected %s but got %s", testTime, expectedEST, formatted)
+	if formatted != expectedFormat {
+		t.Fatalf(errorTemplate, testTime, expectedFormat, formatted)
 	}
 }
